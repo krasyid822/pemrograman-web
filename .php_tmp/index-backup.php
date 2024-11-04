686d96695067403d802af713d71b2affd2cb3b8b
krasyid822 <krasyid822@gmail.com>
2024-11-04 12:55:52 +0700

    .

---
+++ .php_tmp/index-backup.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laundry</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: Inria Sans;
+            height: 2275px;
+            top: -364px;
+            left: -316px;
+            gap: 0px;
+            opacity: 0px;
+
+            background: #FFFFFF;
+        }
+
+        .rectangle-1 {
+            height: 58px;
+        }
+        .logo {
+            width: 34px;
+            height: 35px;
+            top: 29px;
+            left: 76px;
+            gap: 0px;
+            opacity: 0px;
+        }
+        .home-rec-1 {
+            width: 53px;
+            height: 24px;
+            top: 46px;
+            left: 431px;
+            gap: 0px;
+            opacity: 0px;
+
+            font-family: Inria Sans;
+            font-size: 20px;
+            font-weight: 700;
+            line-height: 23.98px;
+            text-align: left;
+
+            color: #000000;
+        }
+        .service-rec-1 {
+            padding-left: 10px;
+
+            font-family: Inria Sans;
+            font-size: 20px;
+            font-weight: 700;
+            line-height: 23.98px;
+            text-align: left;
+
+            color: #000000;
+        }
+        .about-rec-1 {
+            padding-left: 10px;
+            font-family: Inria Sans;
+            font-size: 20px;
+            font-weight: 700;
+            line-height: 23.98px;
+            text-align: left;
+
+            color: #000000;
+        }
+        .rec-3-in-1 {
+            width: 90px;
+            height: 43px;
+            top: 25px;
+            left: 1047px;
+            gap: 0px;
+            border-radius: 5px 0px 0px 0px;
+            border: 1px 0px 0px 0px;
+            opacity: 0px;
+
+            background: #FFFFFF;
+
+            border: 1px solid #FFA845E5
+        }
+        .line-1-in-1 {
+            width: 43.01px;
+            height: 0px;
+            top: 25px;
+            left: 1156px;
+            gap: 0px;
+            border: 1px 0px 0px 0px;
+            opacity: 0px;
+            angle: -88.67 deg;
+
+            border: 1px solid #000000
+        }
+        .rec-4-in-1 {
+            width: 72px;
+            height: 43px;
+            top: 25px;
+            left: 1177px;
+            gap: 0px;
+            border-radius: 5px 0px 0px 0px;
+            border: 1px 0px 0px 0px;
+            opacity: 0px;
+
+            background: #FFFFFF;
+
+            border: 1px solid #FFA845E5
+        }
+        .contact-rec-1 {
+            width: 61px;
+            height: 16px;
+            top: 40px;
+            left: 1064px;
+            gap: 0px;
+            opacity: 0px;
+
+            font-family: Inria Sans;
+            font-size: 13px;
+            font-weight: 700;
+            line-height: 15.59px;
+            text-align: left;
+
+            color: #000000;
+        }
+        .login-rec-1 {
+            width: 35px;
+            height: 17px;
+            top: 40px;
+            left: 1197px;
+            gap: 0px;
+            opacity: 0px;
+
+            font-family: Inria Sans;
+            font-size: 14px;
+            font-weight: 700;
+            line-height: 16.79px;
+            text-align: left;
+
+            color: #000000;
+        }
+
+        .rectangle-2 {
+            background: #AC70506E;
+            background-image: url('rsc\\gif\\laundry.gif');
+        }
+        .txt-rasakan-in-rect-2 {
+            width: 228px;
+            height: 60px;
+            top: 239px;
+            left: 119px;
+            gap: 0px;
+            opacity: 0px;
+
+            font-family: Inria Sans;
+            font-size: 25px;
+            font-weight: 700;
+            line-height: 29.98px;
+            text-align: left;
+
+            color: #ffffff;
+        }
+        .txt-tidak-perlu-in-rect-2 {
+            width: 458px;
+            height: 26px;
+            top: 309px;
+            left: 119px;
+            gap: 0px;
+            opacity: 0px;
+
+            font-family: Inria Sans;
+            font-size: 15px;
+            font-weight: 700;
+            line-height: 17.99px;
+            text-align: left;
+
+            color: #ffffff;
+        }
+        .rec-5-in-2 {
+            color: white;
+            padding: 10px;
+            border-radius: 5px;
+            background: #F17B14;
+            border: 1px solid white
+        }
+    </style>
+
+</head>
+<body>
+    <div class="rectangle-1">
+        <img src="rsc\png\logo.png" class="logo" alt="Logo" style="position:absolute; left: 10px; top: 10px;">
+        <div style="position:absolute; left:20%; top: 25px;">
+            <a href=""><span class="home-rec-1">Home</span></a>
+            <a href=""><span class="service-rec-1">Service</span></a>
+            <a href=""><span class="about-rec-1">About</span></a>
+        </div>
+        <div style="color:white; position: absolute; right: 10px; top:20px;">
+            <button style="border: 2px solid #FFA845E5; background: white;">Contact us</button>_<bc class="line-1-in-1"></bc>_<button style="border: 2px solid #FFA845E5; background: white;">Login</button>
+        </div>
+    </div>
+
+    <div class="rectangle-2">
+        <div style="padding: 40px;">
+            <h1 class="txt-rasakan-in-rect-2">Rasakan Kebersihan 
+            Terbaik</h1>
+            <p class="txt-tidak-perlu-in-rect-2">Tidak perlu khawatir lagi soal cucian! Biarkan kru profesional kami di Freshen Laundry Service mengerjakan tugas Anda.</p>
+            <br>
+            <button class="rec-5-in-2">Contact us</button>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ .php_tmp/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freshen Laundry Service</title>
+
+    <style>
+        body {
+            height: 2275px;
+        }
+        /* Styling untuk header */
+        header {
+            background-color: #333;
+            color: #000;
+            padding: 15px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            position: fixed;
+        }
+        header h1 {
+            margin: 0;
+        }
+        nav {
+            display: flex;
+            gap: 15px;
+        }
+        nav a {
+            color: #000;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        nav a:hover {
+            text-decoration: underline;
+        }
+        .div-1 {
+            right: 10px;
+            position: absolute;
+        }
+        .div-2 {
+            right: 30%;
+            left: 30%;
+            position: absolute;
+        }
+    </style>
+
+</head>
+<body>
+    <nav>
+        <a href="index.php">
+            <img src="rsc\png\logo.png" alt="Freshen Laundry Service" width="30">
+        </a>
+        <div class="div-2">
+            <a href="index.php">Home</a>
+            <a href="services.php">Services</a>
+            <a href="about.php">About</a>
+        </div>
+        <div class="div-1">
+            <a href="contact.php">Contact</a> |
+            <a href="login.php">Login</a>
+        </div>
+    </nav>
+    <div>
+        <img src=""
+    </div>
+</body>
+</html>
